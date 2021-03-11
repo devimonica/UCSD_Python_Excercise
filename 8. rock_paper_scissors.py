bd0b32898d4c1a11939f4768871d55bd3162635d
devimonica <48072621+devimonica@users.noreply.github.com>
2021-03-10 19:26:46 -0500

    Update and rename 8. Rock Paper Scissors to 8. rock_paper_scissors.py

--- 8. rock_paper_scissors.py
+++ 8. rock_paper_scissors.py
@@ -1,13 +1,13 @@
-Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, 
-print out a message of congratulations to the winner, and ask if the players want to start a new game)
+# Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, 
+# print out a message of congratulations to the winner, and ask if the players want to start a new game)
 
-Remember the rules:
+# Remember the rules:
 
-    Rock beats scissors
-    Scissors beats paper
-    Paper beats rock
+#     Rock beats scissors
+#     Scissors beats paper
+#     Paper beats rock
 
-Solution:
+# Solution:
 
 while True:
     player_1 = input('Player 1: \n Rock-Papers-Scissors ?: ')
@@ -31,7 +31,7 @@ while True:
     if input('Do you want to play again ? (y/n): ') == 'n':
         break
         
-Solution 2:
+# Solution 2:
 
 while True:
     player_1 = input('Player 1: \n Rock-Papers-Scissors ?: ')

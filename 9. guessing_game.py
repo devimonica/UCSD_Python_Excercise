859e36ad924152e489d04e82fa9baefc4bab95e6
devimonica <48072621+devimonica@users.noreply.github.com>
2021-03-12 21:16:36 -0500

    Update and rename 9. Guessing game to 9. guessing_game.py

--- 9. guessing_game.py
+++ 9. guessing_game.py
@@ -1,12 +1,12 @@
-Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right. (_Hint: remember to use the user input lessons from the very first exercise
+# Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right. (_Hint: remember to use the user input lessons from the very first exercise
 
-Extras:
+# Extras:
 
-    Keep the game going until the user types “exit”
-    Keep track of how many guesses the user has taken, and when the game ends, print this out.
+#     Keep the game going until the user types “exit”
+#     Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 
-Solution:
+# Solution:
 
 import random
 numbers = range(10)

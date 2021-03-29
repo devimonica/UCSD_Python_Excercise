efb7b600daf4e74a1035f7441efc565c40cb7f35
devimonica <48072621+devimonica@users.noreply.github.com>
2021-03-29 13:39:51 -0400

    Update 21. write_to_a_file.py

--- 21. write_to_a_file.py
+++ 21. write_to_a_file.py
@@ -1,4 +1,19 @@
-# Take the code from the How To Decode A Website exercise (if you didn’t do it or just want to play with some different code, use the code from the solution), and instead of printing the results to a screen, write the results to a txt file. In your code, just make up a name for the file you are saving to.
+# Take the code from the How To Decode A Website exercise (if you didn’t do it or just want to play with some different code, use the code from the solution), 
+# and instead of printing the results to a screen, write the results to a txt file. In your code, just make up a name for the file you are saving to.
+
+# the code from the solution
+# import requests
+# from bs4 import BeautifulSoup
+ 
+# base_url = 'http://www.nytimes.com'
+# r = requests.get(base_url)
+# soup = BeautifulSoup(r.text)
+ 
+# for story_heading in soup.find_all(class_="story-heading"): 
+#     if story_heading.a: 
+#         print(story_heading.a.text.replace("\n", " ").strip())
+#     else: 
+#         print(story_heading.contents[0].strip())
 
 # Extras:
 

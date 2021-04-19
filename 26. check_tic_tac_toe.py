a0fd1026cf650294c26f52ca3821fcae86fe3625
devimonica <48072621+devimonica@users.noreply.github.com>
2021-04-19 19:48:59 -0400

    Update 26. check_tic_tac_toe.py

--- 26. check_tic_tac_toe.py
+++ 26. check_tic_tac_toe.py
@@ -1,1 +1,16 @@
+# This exercise is Part 2 of 4 of the Tic Tac Toe exercise series. The other exercises are: Part 1, Part 3, and Part 4.
 
+# As you may have guessed, we are trying to build up to a full tic-tac-toe board. However, 
+# this is significantly more than half an hour of coding, so we’re doing it in pieces.
+
+# Today, we will simply focus on checking whether someone has WON a game of Tic Tac Toe, 
+# not worrying about how the moves were made.
+
+# If a game of Tic Tac Toe is represented as a list of lists, like so:
+
+# game = [[1, 2, 0],
+# 	[2, 1, 0],
+# 	[2, 1, 1]]
+
+# where a 0 means an empty square, a 1 means that player 1 put their token in that space, 
+# and a 2 means that player 2 put their token in that space.

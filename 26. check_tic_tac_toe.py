786e91d5c39a0ec81beadd40d76f1437593de504
devimonica <48072621+devimonica@users.noreply.github.com>
2021-04-21 23:13:35 -0400

    Update 26. check_tic_tac_toe.py

--- 26. check_tic_tac_toe.py
+++ 26. check_tic_tac_toe.py
@@ -15,21 +15,28 @@
 # where a 0 means an empty square, a 1 means that player 1 put their token in that space, 
 # and a 2 means that player 2 put their token in that space.
 
-Your task this week: given a 3 by 3 list of lists that represents a Tic Tac Toe game board, 
-tell me whether anyone has won, and tell me which player won, if any. A Tic Tac Toe win is 3 in a row - 
-either in a row, a column, or a diagonal. Don’t worry about the case where TWO people have won - 
-assume that in every board there will only be one winner.
+# Your task this week: given a 3 by 3 list of lists that represents a Tic Tac Toe game board, 
+# tell me whether anyone has won, and tell me which player won, if any. A Tic Tac Toe win is 3 in a row - 
+# either in a row, a column, or a diagonal. Don’t worry about the case where TWO people have won - 
+# assume that in every board there will only be one winner.
 
-Here are some more examples to work with:
+# Here are some more examples to work with:
 
-winner_is_2 = [[2, 2, 0],
-	[2, 1, 0],
-	[2, 1, 1]]
+# winner_is_2 = [[2, 2, 0],
+# 	[2, 1, 0],
+# 	[2, 1, 1]]
 
-winner_is_1 = [[1, 2, 0],
-	[2, 1, 0],
-	[2, 1, 1]]
+# winner_is_1 = [[1, 2, 0],
+# 	[2, 1, 0],
+# 	[2, 1, 1]]
 
-winner_is_also_1 = [[0, 1, 0],
-	[2, 1, 0],
-	[2, 1, 1]]
+# winner_is_also_1 = [[0, 1, 0],
+# 	[2, 1, 0],
+# 	[2, 1, 1]]
+# no_winner = [[1, 2, 0],
+# 	[2, 1, 0],
+# 	[2, 1, 2]]
+
+# also_no_winner = [[1, 2, 0],
+# 	[2, 1, 0],
+# 	[2, 1, 0]]

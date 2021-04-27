402a29e537a3dbda6511c7083008b2edbc94dff9
devimonica <48072621+devimonica@users.noreply.github.com>
2021-04-26 21:32:48 -0400

    Update 27. tic_tac_toe_draw.py

--- 27. tic_tac_toe_draw.py
+++ 27. tic_tac_toe_draw.py
@@ -1,1 +1,4 @@
+# This exercise is Part 3 of 4 of the Tic Tac Toe exercise series. The other exercises are: Part 1, Part 2, and Part 4.
 
+# In a previous exercise we explored the idea of using a list of lists as a “data structure” to store information about a tic tac toe game. 
+# In a tic tac toe game, the “game server” needs to know where the Xs and Os are in the board, to know whether player 1 or player 2 (or whoever is X and O won).

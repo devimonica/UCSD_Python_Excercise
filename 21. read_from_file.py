c3b73210c36799be04e6299236816767e78e69b5
devimonica <48072621+devimonica@users.noreply.github.com>
2021-05-05 20:34:46 -0400

    Update 21. read_from_file.py

--- 21. read_from_file.py
+++ 21. read_from_file.py
@@ -7,18 +7,18 @@
 
 # Solution:
   
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
  
-base_url = 'http://www.nytimes.com'
-r = requests.get(base_url)
-soup = BeautifulSoup(r.text)
+# base_url = 'http://www.nytimes.com'
+# r = requests.get(base_url)
+# soup = BeautifulSoup(r.text)
 
-filename = input("File to save to: ")
+# filename = input("File to save to: ")
 
-with open(filename, 'w') as f:
-  for story_heading in soup.find_all(class_="story-heading"): 
-      if story_heading.a: 
-          f.write(story_heading.a.text.replace("\n", " ").strip())
-      else: 
-          f.write(story_heading.contents[0].strip())
+# with open(filename, 'w') as f:
+#   for story_heading in soup.find_all(class_="story-heading"): 
+#       if story_heading.a: 
+#           f.write(story_heading.a.text.replace("\n", " ").strip())
+#       else: 
+#           f.write(story_heading.contents[0].strip())

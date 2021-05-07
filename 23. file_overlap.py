d60f7e5fafb789e2cb69d7e33cca2b915bf3be1b
devimonica <48072621+devimonica@users.noreply.github.com>
2021-05-06 22:18:19 -0400

    Update 23. file_overlap.py

--- 23. file_overlap.py
+++ 23. file_overlap.py
@@ -7,7 +7,7 @@
 # (If you forgot, prime numbers are numbers that can’t be divided by any other number. 
 # And yes, happy numbers are a real thing in mathematics - you can look it up on Wikipedia. The explanation is easier with an example, which I will describe below.)
 
-# Solution:
+# Solution (with for loop):
 
 primeslist = []
 with open('primenumbers.txt') as primesfile:
@@ -28,4 +28,20 @@ for elem in primeslist:
 	if elem in happieslist:
 		overlaplist.append(elem)
 		
+print(overlaplist)
+
+# Solution (with functions using list comprehensions):
+def filetolistofints(filename):
+	list_to_return = []
+	with open(filename) as f:
+		line = f.readline()
+		while line:
+			list_to_return.append(int(line))
+			line = f.readline()
+	return list_to_return
+
+primeslist = filetolistofints('primenumbers.txt')
+happieslist = filetolistofints('happynumbers.txt')
+
+overlaplist = [elem for elem in primeslist if elem in happieslist]
 print(overlaplist)

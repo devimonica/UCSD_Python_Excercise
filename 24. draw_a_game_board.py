20ea59df0fc206030e65a4b149cea09ff7847e1a
devimonica <48072621+devimonica@users.noreply.github.com>
2021-05-10 21:15:08 -0400

    Update 24. draw_a_game_board.py

--- 24. draw_a_game_board.py
+++ 24. draw_a_game_board.py
@@ -21,7 +21,7 @@
 # previously on this blog - https://www.practicepython.org/exercise/2014/04/16/11-check-primality-functions.html
 # elsewhere on the Internet, like this TutorialsPoint link - http://www.tutorialspoint.com/python/python_functions.htm?utm_source=7_&utm_medium=affiliate&utm_content=5f34cd37cdf1050001b09537&utm_campaign=Admitad&utm_term=a5e196742126bebb5adcdb73b91b882c
 
-# Solution:
+# Trivial solution:
 
 a = '---'.join('    ')
 b = '   '.join('||||')

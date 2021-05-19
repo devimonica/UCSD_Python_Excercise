4ef1c69e3b0f358c7ca98b54d49db9e78a6d8791
devimonica <48072621+devimonica@users.noreply.github.com>
2021-05-18 22:24:44 -0400

    Update 28. max_of_three.py

--- 28. max_of_three.py
+++ 28. max_of_three.py
@@ -1,1 +1,3 @@
-Implement a function that takes as input three variables, and returns the largest of the three. Do this without using the Python max() function!
+# Implement a function that takes as input three variables, and returns the largest of the three. Do this without using the Python max() function!
+
+# The goal of this exercise is to think about some internals that Python normally takes care of for us. All you need is some variables and if statements!
